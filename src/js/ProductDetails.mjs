a9f790613881c689d02e0d7f31a5b12d579c7e29
JonathanMazzoli2401 <jonathanmazzoli25@gmail.com>
2026-03-12 21:17:47 -0300

    Completed dynamic product detail page and cart improvements

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,47 @@
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+
+    document
+      .getElementById('addToCart')
+      .addEventListener('click', this.addProductToCart.bind(this));
+  }
+
+  addProductToCart() {
+    let cartItems = getLocalStorage('so-cart') || [];
+
+    const existingProduct = cartItems.find(
+      (item) => item.Id === this.product.Id
+    );
+
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      const productToAdd = { ...this.product, quantity: 1 };
+      cartItems.push(productToAdd);
+    }
+
+    setLocalStorage('so-cart', cartItems);
+  }
+
+  renderProductDetails() {
+    const container = document.querySelector('#product-detail');
+
+    container.innerHTML = `
+      <h2>${this.product.Name}</h2>
+      <img src="${this.product.Image}" alt="${this.product.Name}">
+      <p>${this.product.Description}</p>
+      <p><strong>Price:</strong> $${this.product.FinalPrice}</p>
+      <button id="addToCart">Add to Cart</button>
+    `;
+  }
+}
--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,53 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart') || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
+
+  document.querySelectorAll('.remove-item').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const id = e.target.dataset.id;
+      removeProductFromCart(id);
+    });
+  });
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="cart-card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
+<a href="#" class="cart-card__image">
+<img
+  src="${item.Image}"
+  alt="${item.Name}"
+/>
+</a>
+<a href="#">
+<h2 class="cart-card__name">${item.Name}</h2>
+</a>
+<p class="cart-card__color">${item.Colors[0].ColorName}</p>
+<p class="cart-card__quantity">qty: ${item.quantity || 1}</p>
+<p class="cart-card__price">$${item.FinalPrice}</p>
+<button class="remove-item" data-id="${item.Id}">Remove</button>
 </li>`;
 
   return newItem;
+}
+
+function removeProductFromCart(id) {
+  let cartItems = getLocalStorage('so-cart') || [];
+
+  const productIndex = cartItems.findIndex((item) => item.Id === id);
+
+  if (productIndex !== -1) {
+    if (cartItems[productIndex].quantity && cartItems[productIndex].quantity > 1) {
+      cartItems[productIndex].quantity -= 1;
+    } else {
+      cartItems.splice(productIndex, 1);
+    }
+  }
+
+  setLocalStorage('so-cart', cartItems);
+  renderCartContents();
 }
 
 renderCartContents();
